62a9ea02c30630c359bf422fdd8b1ad976faf223
Sebastian Safari <ssebs@ymail.com>
2019-04-11 10:29:46 -0700

    actual card view

--- frontend/src/components/CardView.js
+++ frontend/src/components/CardView.js
@@ -1,27 +1,33 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Row, Col } from "antd";
 
 const CardView = props => {
     // console.log(props)
     const cards = props.cardData.map(cardItem => {
         return (
-        <Card 
-        key={cardItem.id} 
-        title={cardItem.title}
-        extra={cardItem.author}
-        >
-            {cardItem.summary}
-        </Card>);
+            <Col span={10} key={cardItem.id}>
+                <Card
+                    key={cardItem.id}
+                    title={cardItem.title}
+                    extra={cardItem.author}
+                >
+                    {cardItem.summary}
+                </Card>
+            </Col>
+        );
     });
 
     return (
-    <div>
-    {cards}
-        {/* <Card title='Project 1'>
+        <div>
+            <Row type='flex' justify="center">
+                {cards}
+            </Row>
+
+            {/* <Card title='Project 1'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </Card> */}
-    </div>
-);
-    }
+        </div>
+    );
+};
 export default CardView;
