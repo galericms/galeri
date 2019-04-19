d6edd878c3399fa6e0fad882f1e0c4665c5a732b
Aaron Ang <awz.ang@gmail.com>
2019-04-18 23:07:43 -0700

    Generate Project handler

---
+++ backend/src/Handler/Project.hs
@@ -0,0 +1,9 @@
+module Handler.Project where
+
+import Import
+
+getProjectR :: Handler Html
+getProjectR = error "Not yet implemented: getProjectR"
+
+postProjectR :: Handler Html
+postProjectR = error "Not yet implemented: postProjectR"
---
+++ backend/test/Handler/ProjectSpec.hs
@@ -0,0 +1,14 @@
+module Handler.ProjectSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getProjectR" $ do
+        error "Spec not implemented: getProjectR"
+
+
+    describe "postProjectR" $ do
+        error "Spec not implemented: postProjectR"
+
