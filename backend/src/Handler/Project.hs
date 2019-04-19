b9965063404ada8a97ef89c203c336555a6f6bee
Aaron Ang <awz.ang@gmail.com>
2019-04-19 00:05:51 -0700

    Prototype GET POST /projects

--- backend/src/Handler/Project.hs
+++ backend/src/Handler/Project.hs
@@ -1,9 +1,25 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+
 module Handler.Project where
 
 import Import
 
-getProjectR :: Handler Html
-getProjectR = error "Not yet implemented: getProjectR"
+getProjectR :: Handler Value
+getProjectR = do
+    projects <- runDB $ selectList [] [] :: Handler [Entity Project]
+    sendStatusJSON ok200 $ projects
 
-postProjectR :: Handler Html
-postProjectR = error "Not yet implemented: postProjectR"
+postProjectR :: Handler Value
+postProjectR = do
+    project <- requireJsonBody :: Handler Project
+    projectId <- runDB $ insert project
+    renderUrl <- getUrlRender
+    sendStatusJSON created201 $ object [ "id" .= projectId
+                                       , "title" .= projectTitle project
+                                       , "summary" .= projectSummary project
+                                       , "url" .= renderUrl ProjectR
+                                       ]
