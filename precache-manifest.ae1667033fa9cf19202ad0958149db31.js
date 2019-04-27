cf761eccf22a106c27eded8d84991a39b0862e93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-27 01:19:49 +0000

    Deploy galericms/galeri to github.com/galericms/galeri.git:gh-pages

---
+++ precache-manifest.ae1667033fa9cf19202ad0958149db31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "30dd0f529e5155cab8a1aefa4716de7f",
+    "url": "/static/media/tui-editor.30dd0f52.png"
+  },
+  {
+    "revision": "b4361244b610df3a6c728a26a49f782b",
+    "url": "/static/media/tui-editor-2x.b4361244.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c16c98a1bb8848001d7",
+    "url": "/static/js/main.740ae138.chunk.js"
+  },
+  {
+    "revision": "925d89bc399fd18b3222",
+    "url": "/static/js/2.97e98478.chunk.js"
+  },
+  {
+    "revision": "4c16c98a1bb8848001d7",
+    "url": "/static/css/main.85ea51c0.chunk.css"
+  },
+  {
+    "revision": "925d89bc399fd18b3222",
+    "url": "/static/css/2.d0d0d1aa.chunk.css"
+  },
+  {
+    "revision": "85952ac57fab79ce33802f68293affdd",
+    "url": "/index.html"
+  }
+];
