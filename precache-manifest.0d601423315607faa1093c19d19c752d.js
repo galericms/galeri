d1e66e2884e931eda86c9a3fe4ae86cc5d3de504
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-23 00:31:05 +0000

    Deploy galericms/galeri to github.com/galericms/galeri.git:gh-pages

---
+++ precache-manifest.0d601423315607faa1093c19d19c752d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "30dd0f529e5155cab8a1aefa4716de7f",
+    "url": "/galeri/static/media/tui-editor.30dd0f52.png"
+  },
+  {
+    "revision": "b4361244b610df3a6c728a26a49f782b",
+    "url": "/galeri/static/media/tui-editor-2x.b4361244.png"
+  },
+  {
+    "revision": "fa20862232b180ad7e81",
+    "url": "/galeri/static/js/runtime~main.5eb27fb8.js"
+  },
+  {
+    "revision": "0f5df33fd8c45c0eefeb",
+    "url": "/galeri/static/js/main.96b816d4.chunk.js"
+  },
+  {
+    "revision": "8a18aec4c1165cbb117d",
+    "url": "/galeri/static/js/2.3b021785.chunk.js"
+  },
+  {
+    "revision": "0f5df33fd8c45c0eefeb",
+    "url": "/galeri/static/css/main.f3873898.chunk.css"
+  },
+  {
+    "revision": "8a18aec4c1165cbb117d",
+    "url": "/galeri/static/css/2.1f9afefb.chunk.css"
+  },
+  {
+    "revision": "70c0940ebf036cef5f1349f3a5bcb1e7",
+    "url": "/galeri/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/galeri/precache-manifest.140b8d6b23b3bc3c971eed0c53617cc9.js"
+  "/galeri/precache-manifest.0d601423315607faa1093c19d19c752d.js"
 );
 
 workbox.clientsClaim();
